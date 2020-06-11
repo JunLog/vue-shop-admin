1d23df7c547975d7b1824c5c2088c92c6210466e
junlog <junlog@foxmail.com>
2020-06-11 10:13:58 +0800

    完成订单功能的开发

--- vue_shop/src/components/order/Order.vue
+++ vue_shop/src/components/order/Order.vue
@@ -11,11 +11,7 @@
       <!-- 搜索 添加区域 -->
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input
-            placeholder="请输入内容"
-            v-model="queryInfo.query"
-            clearable
-          >
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable>
             <el-button slot="append" icon="el-icon-search"></el-button>
           </el-input>
         </el-col>
@@ -27,26 +23,21 @@
         <el-table-column label="订单价格" prop="order_price"></el-table-column>
         <el-table-column label="是否付款" prop="pay_status">
           <template slot-scope="scope">
-            <el-tag type="success" v-if="scope.row.pay_status === '1'">
-              已付款
-            </el-tag>
+            <el-tag type="success" v-if="scope.row.pay_status === '1'">已付款</el-tag>
             <el-tag type="danger" v-else>未付款</el-tag>
           </template>
         </el-table-column>
         <el-table-column label="是否发货" prop="is_send"></el-table-column>
         <el-table-column label="下单时间" prop="create_time">
-          <template slot-scope="scope">{{
+          <template slot-scope="scope">
+            {{
             (scope.row.create_time * 1000) | dateFormat
-          }}</template>
+            }}
+          </template>
         </el-table-column>
         <el-table-column label="操作" width="180px">
           <template slot-scope="scope">
-            <el-tooltip
-              effect="dark"
-              content="地址"
-              placement="top"
-              :enterable="false"
-            >
+            <el-tooltip effect="dark" content="地址" placement="top" :enterable="false">
               <el-button
                 type="primary"
                 icon="el-icon-edit"
@@ -55,12 +46,7 @@
               ></el-button>
             </el-tooltip>
 
-            <el-tooltip
-              effect="dark"
-              content="物流"
-              placement="top"
-              :enterable="false"
-            >
+            <el-tooltip effect="dark" content="物流" placement="top" :enterable="false">
               <el-button
                 type="success"
                 icon="el-icon-location"
@@ -83,12 +69,7 @@
       ></el-pagination>
     </el-card>
     <!-- 地址对话框 -->
-    <el-dialog
-      title="修改地址"
-      :visible.sync="addressVisible"
-      width="50%"
-      @close="addressDialogclosed"
-    >
+    <el-dialog title="修改地址" :visible.sync="addressVisible" width="50%" @close="addressDialogclosed">
       <el-form
         :model="addressForm"
         :rules="addressFormRules"
@@ -96,10 +77,7 @@
         label-width="100px"
       >
         <el-form-item label="省市区/县" prop="address1">
-          <el-cascader
-            v-model="addressForm.address1"
-            :options="cityData"
-          ></el-cascader>
+          <el-cascader v-model="addressForm.address1" :options="cityData"></el-cascader>
         </el-form-item>
         <el-form-item label="详细地址" prop="address2">
           <el-input v-model="addressForm.address2"></el-input>
@@ -107,9 +85,7 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="addressVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addressVisible = false"
-          >确 定</el-button
-        >
+        <el-button type="primary" @click="addressVisible = false">确 定</el-button>
       </span>
     </el-dialog>
     <!-- 物流对话框 -->
@@ -119,17 +95,14 @@
       width="50%"
       @close="progressDialogclosed"
     >
-      <el-form ref="progressFormRef" label-width="100px">
-        <!-- <el-form-item label="省市区/县" prop="progress1">
-          <el-cascader
-            v-model="progressForm.progress1"
-            :options="cityData"
-          ></el-cascader>
-        </el-form-item>
-        <el-form-item label="详细地址" prop="progress2">
-          <el-input v-model="progressForm.progress2"></el-input>
-        </el-form-item> -->
-      </el-form>
+      <!-- 时间线 -->
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity, index) in progressInfo"
+          :key="index"
+          :timestamp="activity.time"
+        >{{activity.context}}</el-timeline-item>
+      </el-timeline>
     </el-dialog>
   </div>
 </template>
@@ -212,6 +185,8 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@import '../../plugins/timeline/timeline.css';
+@import '../../plugins/timeline-item/timeline-item.css';
 .el-cascader {
   width: 100%;
 }
--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -40,6 +40,9 @@ import {
   Upload
 } from 'element-ui'
 
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
+
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
@@ -76,6 +79,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
